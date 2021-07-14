e4434ff409c0d1023a979b7d84a0b6c90af8ce26
Steffny Marif Bill <34091850+steffny1@users.noreply.github.com>
2021-07-14 12:06:14 +1000

    Update index.js
    added users endpoint

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ const lib = require("pipedrive");
 const app = express();
 const port = process.env.PORT || 5000;
 
-lib.Configuration.apiToken = "INSERT API KEY HERE";
+lib.Configuration.apiToken = "4aaf3dc71669d84f5198c16d3ad9e7ff7c214ad5";
 
+//middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json({ extended: true }));
 
@@ -26,26 +27,32 @@ app.get("/", async (req, res) => {
   input["assigned_to_user_id"] = 11765267;
   input["filter_id"] = 11765267;
 
-  const activity =
-    await lib.ActivitiesController.getAllActivitiesAssignedToAParticularUser(
-      input,
-      setTimeoutPromise(120000)
-    );
-  res.send(JSON.stringify(activity, null, 4));
+  try {
+    const activity =
+      await lib.ActivitiesController.getAllActivitiesAssignedToAParticularUser(
+        input,
+        setTimeoutPromise(120000)
+      );
+    res.send(JSON.stringify(activity, null, 4));
+  } catch (error) {
+    console.log(error);
+  }
 });
 
-/*
 //get all users
-app.get("/", async (req, res) => {
+app.get("/users", async (req, res) => {
   res.header("Content-Type", "application/json");
 
-  const user = await lib.UsersController.getAllUsers();
-  //res.send(JSON.stringify(user, null, 4));
-  res.send(user);
+  try {
+    const user = await lib.UsersController.getAllUsers();
+    res.send(JSON.stringify(user, null, 4));
+  } catch (error) {
+    console.log(error);
+  }
+
   //"id":11765267,"name":"Paola",
   //"id":11765255,"name":"Paul Kortman
 });
-*/
 
 app.listen(port, (req, res) => {
   console.log(`Server is running on http://localhost:${port}`);
